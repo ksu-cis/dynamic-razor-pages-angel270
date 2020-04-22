b473488b8e09c01f047c7ada0536a2422d671fa9
Angel Hernandez <angel75@ksu.edu>
2020-04-22 14:12:45 -0500

    done! <3

--- Movies/Movie.cs
+++ Movies/Movie.cs
@@ -11,27 +11,27 @@ namespace Movies
     public class Movie
     {
         /// <summary>
-        /// Gets or sets the title of the movie
+        /// Gets and sets the title of the movie
         /// </summary>
         public string Title { get; set; }
-                
+
         /// <summary>
-        /// Gets or sets the MPAA rating of the movie
+        /// Gets and sets the Motion Picture Association of America Rating
         /// </summary>
         public string MPAARating { get; set; }
 
         /// <summary>
-        /// Gets or sets the genre of the movie
+        /// Gets and sets the primary genre of the movie
         /// </summary>
         public string MajorGenre { get; set; }
 
         /// <summary>
-        /// Gets or sets the IMDB rating of the movie
+        /// Gets and sets the Internet Movie Database rating of the movie
         /// </summary>
         public float? IMDBRating { get; set; }
 
         /// <summary>
-        /// Gets or sets the rotten tomatoes rating of the movie
+        /// Gets and sets the Rotten Tomatoes rating of the movie
         /// </summary>
         public float? RottenTomatoesRating { get; set; }
     }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,56 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// The movies to display on the index page 
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
-        public void OnGet()
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>  
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        public double? IMDBMax { get; set; }
+
+        /// <summary>
+        /// The minimum Rotten Tomatoes Rating
+        /// </summary>
+        public double? RTMin { get; set; }
+
+        /// <summary>
+        /// The maximum Rotten Tomatoes Rating
+        /// </summary>
+        public double? RTMax { get; set; }
+
+        public void OnGet(double? IMDBMin, double? IMDBMax, double? RTMin, double? RTMax)
         {
-
+            SearchTerms = Request.Query["SearchTerms"];
+            MPAARatings = Request.Query["MPAARatings"];
+            Genres = Request.Query["Genres"];
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RTMin, RTMax);
         }
     }
 }
